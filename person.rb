d2e0825195a22a2f0d0d2141db2e9942b96f0a2d
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-05-10 16:43:22 -0300

    fiz linters

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(id, name, age = 'Unknown', parent_permission: true)
+  def initialize(id, age, name = 'Unknown', parent_permission: true)
     @id = id
     @name = name
     @age = age
--- test.rb
+++ test.rb
@@ -2,9 +2,9 @@ require_relative 'person'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
 
-person = Person.new(1, 22, 'maximilianus')
+person = Person.new(22, 'maximilianus')
 person.correct_name
-capitalizedperson = CapitalizeDecorator.new(person)
-puts capitalizedperson.correct_name
-capitalizedtrimmedperson = TrimmerDecorator.new(capitalizedPerson)
-puts capitalizedtrimmedperson.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+capitalized_trimmed_person.correct_name
