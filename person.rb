8855ed829c63bcf55a421a617953f307c9e639a7
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-05-12 09:04:25 -0300

    setup associations

--- person.rb
+++ person.rb
@@ -1,7 +1,8 @@
 require './nameable'
+require './rentals'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def initialize(id, age, name = 'Unknown', parent_permission: true)
@@ -10,6 +11,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -18,6 +20,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(date, person)
+    Rental.new(date, self, person)
   end
 
   private
